f366cc34e4d13c621deaf3dc0a78398ea3ab6c97
ilhamday <ilhamyudha00@gmail.com>
2026-01-31 20:20:49 +0700

    Add checkbox assertion
    Assert default condition
    Assert text after checkbox is checked
    Assert text after checkbox is unchecked

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,78 +2,95 @@ from playwright.sync_api import Page, expect
 import time
 import re
 
-def test_homepage_title(page: Page):
-    page.goto("https://quickpizza.grafana.com/browser.php")
-    expect(page).to_have_title("")
+# def test_homepage_title(page: Page):
+#     page.goto("https://quickpizza.grafana.com/browser.php")
+#     expect(page).to_have_title("")
 
-def test_geolocation(page: Page, context):
-    context.set_geolocation({"latitude": 37.77, "longitude": -122.42}) # San Francisco
+# def test_geolocation(page: Page, context):
+#     context.set_geolocation({"latitude": 37.77, "longitude": -122.42}) # San Francisco
 
-    page.goto("https://quickpizza.grafana.com/browser.php")
-    # find a button with "Get geolocation" text
-    page.get_by_role("button", name="Get geolocation").click()
+#     page.goto("https://quickpizza.grafana.com/browser.php")
+#     # find a button with "Get geolocation" text
+#     page.get_by_role("button", name="Get geolocation").click()
     
-    expect(page.locator("#geolocation-info-display")).to_contain_text("37.77")
-    expect(page.locator("#geolocation-info-display")).to_contain_text("-122.42")
+#     expect(page.locator("#geolocation-info-display")).to_contain_text("37.77")
+#     expect(page.locator("#geolocation-info-display")).to_contain_text("-122.42")
 
-# 2 - Locale-info-display
-def test_locale(page: Page):
-    page.goto("https://quickpizza.grafana.com/browser.php")
+# # 2 - Locale-info-display
+# def test_locale(page: Page):
+#     page.goto("https://quickpizza.grafana.com/browser.php")
 
-    #redundant inding it by text "id-ID" then checking if it has text "id-ID". That's circular logic.
-    # expect(page.get_by_text("id-ID")).to_have_text("id-ID")
+#     #redundant inding it by text "id-ID" then checking if it has text "id-ID". That's circular logic.
+#     # expect(page.get_by_text("id-ID")).to_have_text("id-ID")
 
-    expect(page.get_by_text("id-ID")).to_be_visible()
-    # expect(page.locator("#locale-info-display")).to_have_text("id-ID")
+#     expect(page.get_by_text("id-ID")).to_be_visible()
+#     # expect(page.locator("#locale-info-display")).to_have_text("id-ID")
 
-def test_network_online(page: Page):
-    page.goto("https://quickpizza.grafana.com/browser.php")
+# def test_network_online(page: Page):
+#     page.goto("https://quickpizza.grafana.com/browser.php")
 
-    page.get_by_role("button", name="Refresh network status").click()
-    expect(page.get_by_text("Network Status: true")).to_be_visible()
+#     page.get_by_role("button", name="Refresh network status").click()
+#     expect(page.get_by_text("Network Status: true")).to_be_visible()
 
-# emulate offline netwrok
-def test_network_offline(page: Page, context):
-    page.goto("https://quickpizza.grafana.com/browser.php")
+# # emulate offline netwrok
+# def test_network_offline(page: Page, context):
+#     page.goto("https://quickpizza.grafana.com/browser.php")
 
-    # offline
-    context.set_offline(True)
+#     # offline
+#     context.set_offline(True)
     
-    page.get_by_role("button", name="Refresh network status").click()
-    expect(page.get_by_text("Network Status: false")).to_be_visible()
+#     page.get_by_role("button", name="Refresh network status").click()
+#     expect(page.get_by_text("Network Status: false")).to_be_visible()
 
-# timezone
-def test_timezone(browser):
-    context = browser.new_context(timezone_id="Asia/Jakarta")
-    page = context.new_page()
+# # timezone
+# def test_timezone(browser):
+#     context = browser.new_context(timezone_id="Asia/Jakarta")
+#     page = context.new_page()
     
-    page.goto("https://quickpizza.grafana.com/browser.php")
-    expect(page.locator("#timezone-info-display")).to_have_text("Timezone: Asia/Jakarta")
+#     page.goto("https://quickpizza.grafana.com/browser.php")
+#     expect(page.locator("#timezone-info-display")).to_have_text("Timezone: Asia/Jakarta")
     
-    context.close()
+#     context.close()
 
-# User Agent
-def test_user_agent(browser):
-    context = browser.new_context(user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36')
-    page =  context.new_page()
+# # User Agent
+# def test_user_agent(browser):
+#     context = browser.new_context(user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36')
+#     page =  context.new_page()
 
+#     page.goto('https://quickpizza.grafana.com/browser.php')
+#     expect(page.locator('#useragent-info-display')).to_have_text('Your UserAgent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36')
+
+#     context.close()
+
+# # Cookies
+# def test_cookie(page: Page, context):
+#     context.add_cookies([{
+#         "name": "test_cookie_id_001",
+#         "value": "1234value1234",
+#         "domain": ".grafana.com",
+#         "path": "/browser.php"
+#     }])
+
+#     page.goto('https://quickpizza.grafana.com/browser.php')
+
+#     page.get_by_role("button", name="Refresh cookies").click()
+#     #assert cookie for name=value
+#     expect(page.locator("#cookies-info-display")).to_contain_text("test_cookie_id_001=1234value1234")
+
+
+def test_checkbox(page: Page):
     page.goto('https://quickpizza.grafana.com/browser.php')
-    expect(page.locator('#useragent-info-display')).to_have_text('Your UserAgent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36')
 
-    context.close()
+    btn_checkbox = page.get_by_role("checkbox", name="Checkbox test 1")
+    checkbox_text = page.locator("#checkbox-info-display")
 
-# Cookies
-def test_cookie(page: Page, context):
-    context.add_cookies([{
-        "name": "test_cookie_id_001",
-        "value": "1234value1234",
-        "domain": ".grafana.com",
-        "path": "/browser.php"
-    }])
+    # before clicking the checkbox
+    expect(checkbox_text).to_have_text("No interaction")
 
-    page.goto('https://quickpizza.grafana.com/browser.php')
+    # 1st click - checked
+    btn_checkbox.check()
+    expect(checkbox_text).to_have_text("Thanks for checking the box")
 
-    page.get_by_role("button", name="Refresh cookies").click()
-    #assert cookie for name=value
-    expect(page.locator("#cookies-info-display")).to_contain_text("test_cookie_id_001=1234value1234")
-    time.sleep(4)
+    # 2nd click - unchecked
+    btn_checkbox.uncheck()
+    expect(checkbox_text).to_have_text("You've just unchecked the box")
